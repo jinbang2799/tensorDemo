00d135b46cf9320f503e08fc33e336ce24745212
jinbang <jinbang!23>
2019-04-10 11:19:16 +0800

    demo代码提交

--- demo/Demo01.py
+++ demo/Demo01.py
@@ -1,33 +1,57 @@
 import tensorflow as tf
+from tensorflow.keras import layers
+print(tf.__version__)
+print(tf.keras.__version__)
+
+model = tf.keras.Sequential()
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+layers.Dense(32, activation='sigmoid')
+layers.Dense(32, activation=tf.sigmoid)
+layers.Dense(32, kernel_initializer='orthogonal')
+layers.Dense(32, kernel_initializer=tf.keras.initializers.glorot_normal)
+layers.Dense(32, kernel_regularizer=tf.keras.regularizers.l2(0.01))
+layers.Dense(32, kernel_regularizer=tf.keras.regularizers.l1(0.01))
+
+model = tf.keras.Sequential()
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
+             loss=tf.keras.losses.categorical_crossentropy,
+             metrics=[tf.keras.metrics.categorical_accuracy])
+
+
 import numpy as np
-import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+train_x = np.random.random((1000, 72))
+train_y = np.random.random((1000, 10))
 
-# 使用 NumPy 生成假数据(phony data), 总共 100 个点.
-x_data = np.float32(np.random.rand(2, 100))  # 随机输入
-y_data = np.dot([0.100, 0.200], x_data) + 0.300
+val_x = np.random.random((200, 72))
+val_y = np.random.random((200, 10))
 
-# 构造一个线性模型
-#
-b = tf.Variable(tf.zeros([1]))
-W = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
-y = tf.matmul(W, x_data) + b
+model.fit(train_x, train_y, epochs=10, batch_size=100,
+          validation_data=(val_x, val_y))
 
-# 最小化方差
-loss = tf.reduce_mean(tf.square(y - y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
+dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+dataset = dataset.batch(32)
+dataset = dataset.repeat()
+val_dataset = tf.data.Dataset.from_tensor_slices((val_x, val_y))
+val_dataset = val_dataset.batch(32)
+val_dataset = val_dataset.repeat()
 
-# 初始化变量
-init = tf.global_variables_initializer()
+model.fit(dataset, epochs=10, steps_per_epoch=30,
+          validation_data=val_dataset, validation_steps=3)
 
-# 启动图 (graph)
-sess = tf.Session()
-sess.run(init)
+test_x = np.random.random((1000, 72))
+test_y = np.random.random((1000, 10))
+model.evaluate(test_x, test_y, batch_size=32)
+test_data = tf.data.Dataset.from_tensor_slices((test_x, test_y))
+test_data = test_data.batch(32).repeat()
+model.evaluate(test_data, steps=30)
 
-# 拟合平面
-for step in range(0, 201):
-    sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(W), sess.run(b))
+# predict
+result = model.predict(test_x, batch_size=32)
+print(result)
