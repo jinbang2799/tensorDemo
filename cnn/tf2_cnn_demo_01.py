a571cbe63ff87772854d06aaa0c6763961840491
jinbang <jinbang1991@foxmail.com>
2019-04-22 21:57:46 +0800

    demo代码提交

---
+++ cnn/tf2_cnn_demo_01.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+tf.config.gpu.set_per_process_memory_fraction = 0.9
+from tensorflow.python import keras
+from tensorflow.python.keras import layers
+
+# print(tf.__version__)
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+x_train = x_train.reshape((-1, 28, 28, 1))
+x_test = x_test.reshape((-1, 28, 28, 1))
+print(x_train.shape, ' ', y_train.shape)
+print(x_test.shape, ' ', y_test.shape)
+
+import matplotlib.pyplot as plt
+
+plt.imshow(x_train[0].reshape(28, 28))
+plt.show()
+
+class_names = ['0', '1', '2', '3', '4', '5',
+               '6', '7', '8', '9']
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i].reshape(28, 28), cmap=plt.cm.binary)
+    plt.xlabel(class_names[y_train[i]])
+
+model = keras.Sequential()
+
+model.add(layers.Conv2D(input_shape=(x_train.shape[1], x_train.shape[2], x_train.shape[3]),
+                        filters=32, kernel_size=(3, 3), strides=(1, 1), padding='valid',
+                        activation='relu'))
+
+model.add(layers.MaxPool2D(pool_size=(2, 2)))
+
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
+# 分类层
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer=keras.optimizers.Adam(),
+              # loss=keras.losses.CategoricalCrossentropy(),  # 需要使用to_categorical
+              loss=keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+model.summary()
+
+history = model.fit(x_train, y_train, batch_size=64, epochs=5, validation_split=0.1)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.legend(['training', 'valivation'], loc='upper left')
+plt.show()
+
+res = model.evaluate(x_test, y_test)
+
+print(res)
---
+++ tf2/bili_demo_01.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from tensorflow.python import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+print(str(train_images.shape))
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+plt.figure(figsize=(10, 50))
+for i in range(250):
+    plt.subplot(50, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
